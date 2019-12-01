673b3e2f0992e12d900dd6d1c559256a1341f97a
shgini27 <go_shuhrat@yahoo.com>
2019-12-02 00:40:52 +0500

    payment charges bag fix

--- lib/screens/payment/paymentList.dart
+++ lib/screens/payment/paymentList.dart
@@ -48,7 +48,7 @@ class _PaymentListState extends State<PaymentList> {
             ListView.builder(
                 shrinkWrap: true,
                 physics: ClampingScrollPhysics(),
-                itemCount: _payments.chargeList != null ? _payments.chargeList.length : 0,
+                itemCount: _payments.chargeList != null ? 1 : 0,
                 itemBuilder: (context, position) {
                   return Column(
                     children: <Widget>[
